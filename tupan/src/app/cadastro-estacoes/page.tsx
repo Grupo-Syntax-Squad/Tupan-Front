4464a4b64c1ae80c82531a448833afa86bf38799
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2024-09-17 23:25:27 -0300

    func: Adicionando imagens na pasta Assets e novas classes do tailwind

--- tupan/src/app/cadastro-estacoes/page.tsx
+++ tupan/src/app/cadastro-estacoes/page.tsx
@@ -1,24 +1,54 @@
 export default function CadastroEstacoes() {
   return (
-    <div className="cadastro-estacoes">
-      <h1><span>Cadastro de nova estação</span></h1>
-      <label htmlFor="nome">Nome</label>
-      <input type="text" />
-      <label htmlFor="cep">CEP</label>
-      <input type="text" />
-      <label htmlFor="logradouto">Logradouro</label>
-      <input type="text" />
-      <label htmlFor="numero">Número</label>
-      <input type="text" />
-      <label htmlFor="complemento">Complemento</label>
-      <input type="text" />
-      <p>Parâmetros de medição</p>
-      <button>Adicionar</button>
-      <select name="" id=""></select>
-      <p>Parâmetros Adicionados</p>
-      {/* Adicionar os parâmetros */}
-      <button className="cancelar">Cancelar</button>
-      <button className="salvar">Salvar</button>
+    <div className="cadastro-estacoes ">
+      <section className="border border-slate-700 p-20 rounded-3xl">
+        <h1 className="flex justify-center text-2xl"><span>Cadastro de nova estação</span></h1>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" />
+        <div className="flex gap-5 input-container">
+          <div className="flex flex-col ">
+            <label htmlFor="cep">CEP</label>
+            <input type="text" />
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="logradouto">Logradouro</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="flex gap-5">
+          <div className="flex flex-col ">
+            <label htmlFor="numero">Número</label>
+            <input type="text" />
+          </div>
+          <div className="flex flex-col ">
+            <label htmlFor="complemento">Complemento</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="flex gap-5">
+          <div>
+            <p>Parâmetros de medição</p>
+            <select name="parametros" className="border p-2 border-slate-700 rounded-xl" id="">
+              <option value="" disabled>Selecione uma opção</option>
+              <option value="">Temperatura</option>
+              <option value="">Umidade</option>
+              <option value="">Vento</option>
+              <option value="">Chuva</option>
+            </select>
+          </div>
+          <div></div>
+          <button className="bg-lime-500 hover:bg-lime-700 text-white font-bold py-0 px-6 rounded h-10 mt-5">Adicionar</button>
+        </div>
+        <p className="my-2">Parâmetros Adicionados</p>
+        <ul className="my-2">
+          <li>Umidade</li>
+          <li>Temperatura</li>
+        </ul>
+        <div className="flex gap-5">
+          <button className="bg-transparent hover:bg-lime-600 text-lime-600 font-semibold hover:text-white py-2 px-4 border border-lime-600 hover:border-transparent rounded">Cancelar</button>
+          <button className="bg-lime-500 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">Salvar</button>
+        </div>
+      </section>
     </div>
   );
 }
