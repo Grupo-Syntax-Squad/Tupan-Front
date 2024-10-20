88c6db824a26c4a990964908e8bd230516cc9313
Gabriel Reis <gabrieldreisilva4@gmail.com>
2024-10-20 12:47:43 -0300

    mant: corrigindo posicao dos graficos

--- tupan/src/app/inicial/page.tsx
+++ tupan/src/app/inicial/page.tsx
@@ -2,7 +2,7 @@
 
 import { MenuLateral } from '@/components/menu/lateral';
 import { NavTop } from '@/components/nav-top';
-import {LineChart, BarChart, ColumnChart} from '@/components/graficos/export';
+import { LineChart, BarChart, ColumnChart } from '@/components/graficos/export';
 
 const menuData = [
   { nome: 'Estações', path: '/estacoes', icone: 'bx bx-home' },
@@ -13,34 +13,37 @@ const menuData = [
   { nome: 'Logout', path: '/logout', icone: 'bx bx-log-out' },
 ];
 
-export default function Inicial () {
+export default function Inicial() {
   return (
-    <div className="w-screen flex bg-gray-100">
-
+    <div className="w-screen flex bg-gray-100 overflow-x-hidden">
       <div className="w-fit pr-4 min-h-screen">
-
         <MenuLateral menuData={menuData} />
-
       </div>
 
-      <div className="w-full flex flex-col gap-4">
-
+      <div className="w-screen flex flex-col overflow-x-hidden">
         <NavTop nome="Página Inicial" path="Inicial" />
 
         <section className="flex p-3 m-4 rounded">
-
-          <LineChart titulo="Exemplo gráfico temperatura" id='line' legenda="Fatec Prof. Jessen Vidal" />
-
+          <LineChart
+            titulo="Exemplo gráfico temperatura"
+            id="line"
+            legenda="Fatec Prof. Jessen Vidal"
+          />
         </section>
 
-        <section className="grid grid-cols-3 p-3 m-4 rounded">
+        <section className="grid grid-cols-2 p-3 m-4 rounded">
+          <BarChart
+            titulo="Exemplo gráfico temperatura"
+            id="bar"
+            legenda="Fatec Prof. Jessen Vidal"
+          />
 
-          <BarChart titulo="Exemplo gráfico temperatura" id='bar' legenda="Fatec Prof. Jessen Vidal" />
-
-          <ColumnChart titulo="Exemplo gráfico temperatura" id='col' legenda="Fatec Prof. Jessen Vidal" />
-
+          <ColumnChart
+            titulo="Exemplo gráfico temperatura"
+            id="col"
+            legenda="Fatec Prof. Jessen Vidal"
+          />
         </section>
-
       </div>
     </div>
   );
